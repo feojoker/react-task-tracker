e834d989cd176f4b6925be79b50dd78a2fdf9f8b
feojoker <feojoker@gmail.com>
2021-03-23 09:33:55 +0300

    add delete task and reminder ui

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,23 @@ function App() {
   // Delete task
 
   const deleteTask = (id) => {
-    console.log('delete')
+    setTasks(tasks.filter((task) => task.id !== id))
+    console.log('delete', id)
+  }
+
+
+  // Toggle Reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    console.log(id)
   }
 
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   )
 }
